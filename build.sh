5531a3f2c5c71bb474f4770e0c5a0a24cd089779
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2020-11-30 00:45:17 +0800

    remove unpopular system

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ OUTPUT_DIR="output"
 PLATFORMS="darwin/amd64"                         # amd64 only as of go1.5
 PLATFORMS="$PLATFORMS windows/amd64 windows/386" # arm compilation not available for Windows
 PLATFORMS="$PLATFORMS linux/amd64 linux/386"
-PLATFORMS="$PLATFORMS linux/ppc64 linux/ppc64le"
-PLATFORMS="$PLATFORMS linux/mips64 linux/mips64le" # experimental in go1.6
-PLATFORMS="$PLATFORMS freebsd/amd64"
-PLATFORMS="$PLATFORMS netbsd/amd64"          # amd64 only as of go1.6
-PLATFORMS="$PLATFORMS openbsd/amd64"         # amd64 only as of go1.6
-PLATFORMS="$PLATFORMS dragonfly/amd64"       # amd64 only as of go1.5
-PLATFORMS="$PLATFORMS plan9/amd64 plan9/386" # as of go1.4
-PLATFORMS="$PLATFORMS solaris/amd64"         # as of go1.3
+#PLATFORMS="$PLATFORMS linux/ppc64 linux/ppc64le"
+#PLATFORMS="$PLATFORMS linux/mips64 linux/mips64le" # experimental in go1.6
+#PLATFORMS="$PLATFORMS freebsd/amd64"
+#PLATFORMS="$PLATFORMS netbsd/amd64"          # amd64 only as of go1.6
+#PLATFORMS="$PLATFORMS openbsd/amd64"         # amd64 only as of go1.6
+#PLATFORMS="$PLATFORMS dragonfly/amd64"       # amd64 only as of go1.5
+#PLATFORMS="$PLATFORMS plan9/amd64 plan9/386" # as of go1.4
+#PLATFORMS="$PLATFORMS solaris/amd64"         # as of go1.3
 PLATFORMS="$PLATFORMS linux/arm64"
 
 for PLATFORM in $PLATFORMS; do
@@ -26,18 +26,18 @@ for PLATFORM in $PLATFORMS; do
   eval $CMD || FAILURES="${FAILURES} ${PLATFORM}"
 done
 
-# ARM builds
-PLATFORMS_ARM="linux freebsd netbsd"
-for GOOS in $PLATFORMS_ARM; do
-  GOARCH="arm"
-  # build for each ARM version
-  for GOARM in 7 6 5; do
-    BIN_FILENAME="${OUTPUT_DIR}/${NAME}-${GOOS}-${GOARCH}${GOARM}"
-    CMD="GOARM=${GOARM} GOOS=${GOOS} GOARCH=${GOARCH} go build -v -ldflags \"-s -w -extldflags '-static'\" -o ${BIN_FILENAME} $@"
-    echo "${CMD}"
-    eval "${CMD}" || FAILURES="${FAILURES} ${GOOS}/${GOARCH}${GOARM}"
-  done
-done
+## ARM builds
+#PLATFORMS_ARM="linux freebsd netbsd"
+#for GOOS in $PLATFORMS_ARM; do
+#  GOARCH="arm"
+#  # build for each ARM version
+#  for GOARM in 7 6 5; do
+#    BIN_FILENAME="${OUTPUT_DIR}/${NAME}-${GOOS}-${GOARCH}${GOARM}"
+#    CMD="GOARM=${GOARM} GOOS=${GOOS} GOARCH=${GOARCH} go build -v -ldflags \"-s -w -extldflags '-static'\" -o ${BIN_FILENAME} $@"
+#    echo "${CMD}"
+#    eval "${CMD}" || FAILURES="${FAILURES} ${GOOS}/${GOARCH}${GOARM}"
+#  done
+#done
 
 cp application.yml "${OUTPUT_DIR}/application.yml"
 cp -r static "${OUTPUT_DIR}/static" # https://github.com/ProtobufBot/Client-UI 前端编译产物dist
