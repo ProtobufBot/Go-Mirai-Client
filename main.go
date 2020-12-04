43bf9fe34fe5236458c0b5fe887947959428579b
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2020-12-05 04:10:32 +0800

    add log time

--- main.go
+++ main.go
@@ -12,6 +12,13 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+func init() {
+	customFormatter := new(log.TextFormatter)
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	customFormatter.FullTimestamp = true
+	log.SetFormatter(customFormatter)
+}
+
 func main() {
 	port := "9000"
 	conf, err := LoadConfig("application.yml")
