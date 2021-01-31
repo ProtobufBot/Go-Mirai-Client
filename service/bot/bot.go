3118354cc9104c10a57e439e4e56fc558ab75ce9
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2021-01-31 16:41:50 +0800

    update init device

--- service/bot/bot.go
+++ service/bot/bot.go
@@ -2,11 +2,11 @@ package bot
 
 import (
 	"fmt"
+	"github.com/Mrs4s/MiraiGo/utils"
 	"io/ioutil"
 	"time"
 
 	"github.com/Mrs4s/MiraiGo/client"
-	"github.com/Mrs4s/MiraiGo/utils"
 	"github.com/ProtobufBot/Go-Mirai-Client/pkg/util"
 	"github.com/fanliao/go-promise"
 	log "github.com/sirupsen/logrus"
@@ -15,26 +15,31 @@ import (
 var Cli *client.QQClient
 
 func InitDevice(path string) {
-	if !util.PathExists(path) {
-		log.Warn("虚拟设备信息不存在, 将自动生成随机设备.")
-		client.GenRandomDevice()
-		client.SystemDeviceInfo.Display = []byte("GMC." + utils.RandomStringRange(6, "0123456789") + ".001")
-		client.SystemDeviceInfo.FingerPrint = []byte("pbbot/gmc/gmc:10/PBBOT.200324.001/" + utils.RandomStringRange(7, "0123456789") + ":user/release-keys")
-		client.SystemDeviceInfo.ProcVersion = []byte("Linux 5.4.0-54-generic" + utils.RandomString(8) + " (android-build@gmail.com)")
-		client.SystemDeviceInfo.AndroidId = client.SystemDeviceInfo.Display
-		client.SystemDeviceInfo.Device = []byte("gmc")
-		client.SystemDeviceInfo.Board = []byte("gmc")
-		client.SystemDeviceInfo.Model = []byte("gmc")
-		client.SystemDeviceInfo.Brand = []byte("pbbot")
-		client.SystemDeviceInfo.Product = []byte("gmc")
-		client.SystemDeviceInfo.Protocol = client.IPad
-		_ = ioutil.WriteFile(path, client.SystemDeviceInfo.ToJson(), 0644)
-		log.Infof("已生成设备信息并保存到 %s 文件.", path)
-	}
-	log.Infof("将使用 %s 内的设备信息运行", path)
+	log.Info("生成随机设备信息")
+	client.GenRandomDevice()
+	client.SystemDeviceInfo.Display = []byte("GMC." + utils.RandomStringRange(6, "0123456789") + ".001")
+	client.SystemDeviceInfo.FingerPrint = []byte("pbbot/gmc/gmc:10/PBBOT.200324.001/" + utils.RandomStringRange(7, "0123456789") + ":user/release-keys")
+	client.SystemDeviceInfo.ProcVersion = []byte("Linux 5.4.0-54-generic" + utils.RandomString(8) + " (android-build@gmail.com)")
+	client.SystemDeviceInfo.AndroidId = client.SystemDeviceInfo.Display
+	client.SystemDeviceInfo.Device = []byte("gmc")
+	client.SystemDeviceInfo.Board = []byte("gmc")
+	client.SystemDeviceInfo.Model = []byte("gmc")
+	client.SystemDeviceInfo.Brand = []byte("pbbot")
+	client.SystemDeviceInfo.Product = []byte("gmc")
+	client.SystemDeviceInfo.Protocol = client.IPad
 	client.SystemDeviceInfo.IpAddress = []byte{192, 168, 31, 101}
-	if err := client.SystemDeviceInfo.ReadJson([]byte(util.ReadAllText(path))); err != nil {
-		util.FatalError(fmt.Errorf("failed to load device info, err: %+v", err))
+
+	if util.PathExists(path) {
+		log.Infof("使用 %s 内的设备信息覆盖随机设备信息", path)
+		if err := client.SystemDeviceInfo.ReadJson([]byte(util.ReadAllText(path))); err != nil {
+			util.FatalError(fmt.Errorf("failed to load device info, err: %+v", err))
+		}
+	}
+
+	log.Infof("保存设备信息到文件 %s", path)
+	err := ioutil.WriteFile(path, client.SystemDeviceInfo.ToJson(), 0644)
+	if err != nil {
+		log.Warnf("写设备信息文件 %s 失败", path)
 	}
 }
 
