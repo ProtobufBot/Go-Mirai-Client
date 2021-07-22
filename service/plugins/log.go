60592d5586ed3051d573d4eff37fd55103f0292f
lz1998 <875543533@qq.com>
2021-07-22 13:25:10 +0800

    update log

--- service/plugins/log.go
+++ service/plugins/log.go
@@ -4,17 +4,17 @@ import (
 	"github.com/Mrs4s/MiraiGo/client"
 	"github.com/Mrs4s/MiraiGo/message"
 	"github.com/ProtobufBot/Go-Mirai-Client/pkg/plugin"
-	"github.com/ProtobufBot/Go-Mirai-Client/pkg/util"
+	"github.com/ProtobufBot/Go-Mirai-Client/service/bot"
 	log "github.com/sirupsen/logrus"
 )
 
 func LogPrivateMessage(cli *client.QQClient, event *message.PrivateMessage) int32 {
-	log.Infof("Bot(%+v) Private(%+v) -> %+v\n", cli.Uin, event.Sender.Uin, util.MustMarshal(event))
+	log.Infof("Bot(%+v) Private(%+v) -> %+v\n", cli.Uin, event.Sender.Uin, bot.MiraiMsgToRawMsg(cli, event.Elements))
 	return plugin.MessageIgnore
 }
 
 func LogGroupMessage(cli *client.QQClient, event *message.GroupMessage) int32 {
 	cli.MarkGroupMessageReaded(event.GroupCode, int64(event.Id)) // 标记为已读，可能可以减少风控
-	log.Infof("Bot(%+v) Group(%+v) -> %+v\n", cli.Uin, event.GroupCode, util.MustMarshal(event))
+	log.Infof("Bot(%+v) Group(%+v) -> %+v\n", cli.Uin, event.GroupCode, bot.MiraiMsgToRawMsg(cli, event.Elements))
 	return plugin.MessageIgnore
 }
