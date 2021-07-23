ae73e45275e46b2d5b5b5672df584b56c630bc96
lz1998 <875543533@qq.com>
2021-07-23 22:28:19 +0800

    file log

--- go.mod
+++ go.mod
@@ -10,13 +10,18 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da
 	github.com/golang/protobuf v1.5.2
 	github.com/gorilla/websocket v1.4.2
+	github.com/jonboulle/clockwork v0.2.2 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/lestrrat-go/strftime v1.0.5 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/pkg/errors v0.9.1
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/sirupsen/logrus v1.8.1
 	github.com/smartystreets/goconvey v1.6.4 // indirect
+	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
--- pkg/gmc/gmc.go
+++ pkg/gmc/gmc.go
@@ -7,8 +7,10 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"path"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/ProtobufBot/Go-Mirai-Client/pkg/config"
 	"github.com/ProtobufBot/Go-Mirai-Client/pkg/gmc/handler"
@@ -16,7 +18,10 @@ import (
 	"github.com/ProtobufBot/Go-Mirai-Client/pkg/util"
 
 	"github.com/gin-gonic/gin"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
 	log "github.com/sirupsen/logrus"
+	easy "github.com/t-tomalak/logrus-easy-formatter"
 )
 
 var (
@@ -41,6 +46,11 @@ func init() {
 	flag.StringVar(&auth, "auth", "", "http basic auth: 'username,password'")
 	flag.Parse()
 
+	InitLog()
+}
+
+func InitLog() {
+	// 输出到命令行
 	customFormatter := &log.TextFormatter{
 		TimestampFormat: "2006-01-02 15:04:05",
 		FullTimestamp:   true,
@@ -48,6 +58,30 @@ func init() {
 	}
 	log.SetFormatter(customFormatter)
 	log.SetOutput(os.Stdout)
+
+	// 输出到文件
+	rotateLogs, err := rotatelogs.New(path.Join("logs", "%Y-%m-%d.log"),
+		rotatelogs.WithLinkName(path.Join("logs", "latest.log")), // 最新日志软链接
+		rotatelogs.WithRotationTime(time.Hour*24),                // 每天一个新文件
+		rotatelogs.WithMaxAge(time.Hour*24*3),                    // 日志保留3天
+	)
+	if err != nil {
+		util.FatalError(err)
+		return
+	}
+	log.AddHook(lfshook.NewHook(
+		lfshook.WriterMap{
+			log.InfoLevel:  rotateLogs,
+			log.WarnLevel:  rotateLogs,
+			log.ErrorLevel: rotateLogs,
+			log.FatalLevel: rotateLogs,
+			log.PanicLevel: rotateLogs,
+		},
+		&easy.Formatter{
+			TimestampFormat: "2006-01-02 15:04:05",
+			LogFormat:       "[%time%] [%lvl%]: %msg% \n",
+		},
+	))
 }
 
 func Start() {
